8b16aa097ff67112723aff2398068c3bef3c8359
kishanthz <rskishanth88@gmail.com>
2025-05-15 11:09:02 +0530

    Add files via upload

---
+++ kish_nm.py
@@ -0,0 +1,37 @@
+# prompt: Recognizing Handwritten Digits 
+
+import tensorflow as tf
+
+# Load the MNIST dataset
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Normalize pixel values to be between 0 and 1
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Define the model
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10)
+])
+
+# Compile the model
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+model.compile(optimizer='adam',
+              loss=loss_fn,
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(x_train, y_train, epochs=5)
+
+# Evaluate the model
+model.evaluate(x_test,  y_test, verbose=2)
+
+# Make predictions (example)
+probability_model = tf.keras.Sequential([
+  model,
+  tf.keras.layers.Softmax()
+])
+predictions = probability_model.predict(x_test)
